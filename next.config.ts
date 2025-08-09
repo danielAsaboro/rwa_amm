071a73589080c0cc1d1292cb1e2e33d3aa8c6826
Daniel Asaboro <asaborodaniel@gmail.com>
2025-08-09 18:40:17 +0100

    fixed lint issues for deploy ment

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
     // Skip ESLint during production builds to avoid blocking on lint errors
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { webpack }) => {
+    // Ignore optional dependency pulled in by pino to avoid bundling errors
+    config.plugins = config.plugins || []
+    config.plugins.push(
+      new webpack.IgnorePlugin({ resourceRegExp: /^pino-pretty$/ })
+    )
+    return config
+  },
 }
 
 export default nextConfig
