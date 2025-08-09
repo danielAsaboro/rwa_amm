d288bc664eeadfec04bcf94e549dbaf08fc8f8be
Daniel Asaboro <asaborodaniel@gmail.com>
2025-08-09 17:32:14 +0100

    switched to rwa from counter

--- anchor/src/counter-exports.ts
+++ anchor/src/counter-exports.ts
@@ -1,18 +1,18 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
 import { AnchorProvider, Program } from '@coral-xyz/anchor'
 import { Cluster, PublicKey } from '@solana/web3.js'
-import CounterIDL from '../target/idl/counter.json'
-import type { Counter } from '../target/types/counter'
+import RWAIDL from '../target/idl/rwa_amm.json'
+import type { RwaAmm } from '../target/types/rwa_amm'
 
 // Re-export the generated IDL and type
-export { Counter, CounterIDL }
+export { RwaAmm, RWAIDL as CounterIDL }
 
 // The programId is imported from the program IDL.
-export const COUNTER_PROGRAM_ID = new PublicKey(CounterIDL.address)
+export const COUNTER_PROGRAM_ID = new PublicKey(RWAIDL.address)
 
 // This is a helper function to get the Counter Anchor program.
-export function getCounterProgram(provider: AnchorProvider, address?: PublicKey): Program<Counter> {
-  return new Program({ ...CounterIDL, address: address ? address.toBase58() : CounterIDL.address } as Counter, provider)
+export function getCounterProgram(provider: AnchorProvider, address?: PublicKey): Program<RWA> {
+  return new Program({ ...RWAIDL, address: address ? address.toBase58() : RWAIDL.address } as RWA, provider)
 }
 
 // This is a helper function to get the program ID for the Counter program depending on the cluster.
